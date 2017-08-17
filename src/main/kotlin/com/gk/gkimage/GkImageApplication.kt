c8588367055bb35109f86412d263865f456cfc25
conggao <578422314@qq.com>
2017-08-17 17:51:32 +0800

    Initial commit

---
+++ src/main/kotlin/com/gk/gkimage/GkImageApplication.kt
@@ -0,0 +1,11 @@
+package com.gk.gkimage
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class GkImageApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(GkImageApplication::class.java, *args)
+}
